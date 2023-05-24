a276f803f67dad064468a024d2e5ad6baed1e934
Erhan FIRAT <erhan.firat@cytosmart.com>
2023-05-24 20:53:16 +0300

    react versions updated

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import App from './App';
 
 
 // Routeların çalışması için <App /> öğesini düzenlemeniz gerekir
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
